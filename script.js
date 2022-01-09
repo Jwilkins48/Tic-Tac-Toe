a76584afa09b5f13a3e78032497fd70207227a6d
Jenna <jenna48071@gmail.com>
2022-01-09 17:00:51 -0600

    Display player markers when clicked

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const gameContainer = document.getElementById('gameBoardContainer');
-const cells = document.querySelectorAll('[data-cell]')
+const cells = document.querySelectorAll('[data-cell-index]')
+const cell = document.querySelectorAll('.cell')
 const gameScore = document.getElementById('gameScore');
 const resetBtn = document.getElementById('resetBtn')
+
+let startingGame = false;
+let gameStatus = 'game starting';
+let winner;
 
 const GameBoard = (() => {
     // Create players
     const Player = (name, marker, turn) => {
-        const getName = () => name;
-        const getMarker = () => marker;
         return {
             name,
             marker,
@@ -17,42 +20,59 @@ const GameBoard = (() => {
 
     const player1 = Player('player1', 'X', true);
     const player2 = Player('player2', 'O', false);
-    let winner = '';
 
     const changePlayerTurn = () => {
-        if(player1.turn == true){
-            player1.turn = false;
-        }else{
-            player1.turn = true;
+        if (currentPlayer == player1.marker) {
+            currentPlayer = player2.marker
+        } else {
+            currentPlayer = player1.marker
         }
     }
 
-    gameDisplay = [];
-    const winningCombo = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+    gameDisplay = ['', '', '', '', '', '', '', '', '', ''];
+    // const winningCombo = [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6]
+    // ]
 
-    const playerTurn = () => {
-        cells.forEach(cells => {
-            cells.addEventListener('click', e => {
-                if (player1.turn === true && e.target.textContent == '') {
-                    gameDisplay[e.target.id] = player1.marker;
 
-                    cells.textContent = player1.marker;
-                    cells.style.color = '#f1b7ce';
+    // const startGame = () => {
+    currentPlayer = player1.marker;
 
-                    changePlayerTurn();
-                }
-            })
+    cells.forEach(cells => {
+        cells.addEventListener('click', e => {
+            if (e.target.textContent == '' && gameStatus == 'game starting') {
+                gameDisplay[e.target.id] = currentPlayer;
+
+                gameDisplay[e.target.id] = currentPlayer;
+                console.log(gameDisplay);
+
+                cells.textContent = currentPlayer;
+                cells.style.color = '#f1b7ce';
+                changePlayerTurn();
+                playerWins();
+
+            }
         })
+    })
+
+    function playerWins() {
+        if (cells[0].innerHTML == cells[1].innerHTML &&
+            cells[1].innerHTML == cells[2].innerHTML &&
+            cells[0].innerHTML.trim() != "") {
+            console.log('Winner!');
+            gameStatus = 'game over';
+        }
     }
+    // }
+
 
 
 });
+GameBoard();
