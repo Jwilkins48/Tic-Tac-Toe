0a3850251f489b3b52479e923ddd9d1b1b485924
Jenna <jenna48071@gmail.com>
2022-01-10 15:28:58 -0600

    added styling for player 2 win

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cell = document.querySelectorAll('.cell')
 const gameScore = document.getElementById('gameScore');
 const resetBtn = document.getElementById('resetBtn')
 
+let header = document.getElementById('header');
 let startingGame = false;
 let gameStatus = 'game starting';
 let winner;
@@ -22,32 +23,34 @@ const GameBoard = (() => {
     const player2 = Player('player2', 'O', false);
 
     const changePlayerTurn = () => {
-        if (currentPlayer == player1.marker) {
-            currentPlayer = player2.marker
+        if (currentPlayer == player1) {
+            currentPlayer = player2
         } else {
-            currentPlayer = player1.marker
+            currentPlayer = player1
         }
     }
 
     gameDisplay = ['', '', '', '', '', '', '', '', '', ''];
 
     // const startGame = () => {
-    currentPlayer = player1.marker;
+    currentPlayer = player1;
 
     cells.forEach(cells => {
         cells.addEventListener('click', e => {
             if (e.target.textContent == '' && gameStatus == 'game starting') {
                 gameDisplay[e.target.id] = currentPlayer;
                 console.log(gameDisplay);
+                cells.classList.add(currentPlayer.marker);
 
-                cells.textContent = currentPlayer;
-                (currentPlayer == player1.marker) ? cells.style.color = '#f1b7ce': cells.style.color = '#8cecae';
+                cells.textContent = currentPlayer.marker;
+                (currentPlayer == player1) ? cells.style.color = '#f1b7ce': cells.style.color = '#8cecae';
                 changePlayerTurn();
                 playerWins();
 
             }
         })
     })
+
 
     function playerWins() {
         if (cells[0].innerHTML == cells[1].innerHTML &&
@@ -82,17 +85,26 @@ const GameBoard = (() => {
         } else if (
             cells[2].innerHTML == cells[4].innerHTML &&
             cells[4].innerHTML == cells[6].innerHTML &&
-            cells[2].innerHTML.trim() != "") {   
+            cells[2].innerHTML.trim() != "") {
             winnerDisplay(2, 4, 6);
         }
 
-        function winnerDisplay(a,b,c) {
-            cells[a].style.color = 'white';
-            cells[a].style.background = "#f1b7ce";
-            cells[b].style.color = 'white';
-            cells[b].style.background = "#f1b7ce";
-            cells[c].style.color = 'white';
-            cells[c].style.background = "#f1b7ce";
+        function winnerDisplay(a, b, c) {
+            if (currentPlayer == player2) {
+                cells[a].style.color = 'white';
+                cells[a].style.background = "#f1b7ce";
+                cells[b].style.color = 'white';
+                cells[b].style.background = "#f1b7ce";
+                cells[c].style.color = 'white';
+                cells[c].style.background = "#f1b7ce";
+            } else {
+                cells[a].style.color = 'white';
+                cells[a].style.background = "#8cecae";
+                cells[b].style.color = 'white';
+                cells[b].style.background = "#8cecae";
+                cells[c].style.color = 'white';
+                cells[c].style.background = "#8cecae";
+            }
             console.log('Winner!');
             gameStatus = 'game over';
         }
